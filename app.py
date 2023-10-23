6ed6ec61d1ae6ee00df56641cd46f04b66dfe1c3
Seongmin Kim <mr.seongminkim@gmail.com>
2023-10-23 12:47:19 +0900

    temporal backups

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import pandas as pd
+#import pandas as pd
 from flask import Flask, render_template, jsonify, request
 
-from nn import run_model
+#from nn import run_model
 
 deberta_v3_large = '/kaggle/input/deberta-v3-large-hf-weights'
 
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+'''
 @app.route('/mcq', methods=['POST'])
 def mcq():
     data = request.get_json()
@@ -19,6 +20,8 @@ def mcq():
     df = pd.DataFrame.from_dict([data], orient="columns")
     answer = run_model(df)
     return jsonify({'answer': answer})
+'''
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=4200, debug=False)
+#10.11.60.88
