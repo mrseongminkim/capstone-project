68543c6925c8ff7264d2cb8421146c405aec231c
Seongmin Kim <mr.seongminkim@gmail.com>
2023-10-26 23:23:06 +0900

    [update] Use http port

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def mcq():
     return jsonify({'answer': answer, 'prob': prob})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4200, debug=False)
+    app.run(host='0.0.0.0', port=80, debug=False)
