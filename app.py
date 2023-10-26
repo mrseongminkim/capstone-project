9ce5b0181c74addb336db7d3d25db11f52da079c
SimMinhyumg <cessio@naver.com>
2023-10-26 18:41:47 +0900

    10.26_fovicon등

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, jsonify, request
 
 from nn import run_model
 
+# from neural_network import infer
 deberta_v3_large = '/kaggle/input/deberta-v3-large-hf-weights'
 
 app = Flask(__name__)
@@ -14,11 +15,11 @@ def index():
 @app.route('/mcq', methods=['POST'])
 def mcq():
     data = request.get_json()
-    print(data)
-    print(type(data))
     df = pd.DataFrame.from_dict([data], orient="columns")
     answer = run_model(df)
-    return jsonify({'answer': answer})
-
+    return jsonify({'answer': answer, 'prob' : '0.99'})
+    # answer, prob = infer(df)
+    # return jsonify({'answer' : answer, 'prob' : prob})
+    
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
