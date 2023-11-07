5033bdfbcab3012e2900b89959afc5693a62ac15
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2023-11-07 19:39:11 +0900

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -10,23 +10,31 @@ document.addEventListener("DOMContentLoaded", function () {
   const selectedAnswer = document.getElementById("answer");
   const loadingElement = document.getElementById("loading");
   const prob = document.getElementById("prob");
-  const imageElement = document.getElementById("image");
-  let loadingInterval;
+  const smileImage = document.getElementById("smile_image");
+  const expressionlessImage = document.getElementById("expressionless_image");
+  const angryImage = document.getElementById("angry_image");
 
-  imageElement.style.display = "none";
   const updateImage = (probability) => {
     if (probability >= 0.8) {
-      imageElement.src = "/static/images/smile.png";
+      smileImage.style.opacity = 1;
+      expressionlessImage.style.opacity = 0.1;
+      angryImage.style.opacity = 0.1;
     } else if (probability >= 0.5) {
-      imageElement.src = "/static/images/expressionless.png";
+      smileImage.style.opacity = 0.1;
+      expressionlessImage.style.opacity = 1;
+      angryImage.style.opacity = 0.1;
     } else {
-      imageElement.src = "/static/images/angry.png";
+      smileImage.style.opacity = 0.1;
+      expressionlessImage.style.opacity = 0.1;
+      angryImage.style.opacity = 1;
     }
   };
   const handleSubmit = async () => {
     selectedAnswer.innerHTML = "";
-    prob.innerHTML = "";
-    imageElement.style.display = "none";
+    loadingElement.style.display = "none";
+    smileImage.style.opacity = 1;
+    expressionlessImage.style.opacity = 1;
+    angryImage.style.opacity = 1;
     const data = {
       question: question.value,
       A: A.value,
@@ -44,33 +52,14 @@ document.addEventListener("DOMContentLoaded", function () {
     if (D.value === "") emptyFieldCount++;
     if (E.value === "") emptyFieldCount++;
     if (question.value === "") {
-      selectedAnswer.innerHTML = "문제를 입력하세요.";
+      selectedAnswer.innerHTML = "<h1>문제를 입력하세요.</h1>";
     } else {
       if (emptyFieldCount === 5) {
-        selectedAnswer.innerHTML = "객관식 답을 입력하세요.";
+        selectedAnswer.innerHTML = "<h1>객관식 답을 입력하세요.</h1>";
       } else if (emptyFieldCount >= 4) {
-        selectedAnswer.innerHTML = "객관식 답을 2개 이상 입력하세요.";
+        selectedAnswer.innerHTML = "<h1>객관식 답을 2개 이상 입력하세요.</h1>";
       } else {
         loadingElement.style.display = "block";
-
-        // 진행 중 타이머 clear
-        if (loadingInterval) {
-          clearInterval(loadingInterval);
-        }
-
-        // 로딩바 %초기화
-        resetLoadingBar();
-        document.getElementById("loading").classList.remove("hidden");
-
-        let currentPercentage = 0;
-
-        loadingInterval = setInterval(function () {
-          currentPercentage++;
-          updateLoadingBarAndPercentage(currentPercentage);
-          if (currentPercentage >= 100) {
-            clearInterval(loadingInterval);
-          }
-        }, 300);
 
         try {
           const res = await fetch("/mcq", {
@@ -81,18 +70,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
           if (res.ok) {
             const val = await res.json();
-            selectedAnswer.innerHTML = `<h2>선택된 답은 ${val.answer}</h2>`;
-            prob.innerHTML = "Probability : " + val.prob;
+            selectedAnswer.innerHTML = `<h1>선택된 답은 ${val.answer}</h1>`;
             updateImage(val.prob);
-            imageElement.style.display = "block";
           } else {
             selectedAnswer.innerHTML =
-              "<p>Error occurred while fetching the answer.</p>";
+              "<h2>Error occurred while fetching the answer.</h2>";
           }
         } catch (error) {
           console.error("에러가 발생했습니다: ", error);
           selectedAnswer.innerHTML =
-            "<p>Error occurred while fetching the answer.</p>";
+            "<h1>Error occurred while fetching the answer.</h1>";
         } finally {
           loadingElement.style.display = "none";
         }
@@ -109,18 +96,66 @@ document.addEventListener("DOMContentLoaded", function () {
     E.value = "";
     selectedAnswer.innerHTML = "";
     prob.innerHTML = "";
-    imageElement.style.display = "none";
+    loadingElement.style.display = "none";
+    smileImage.style.opacity = 1;
+    expressionlessImage.style.opacity = 1;
+    angryImage.style.opacity = 1;
   };
 
-  function updateLoadingBarAndPercentage(percentage) {
-    document.getElementById("loadingBar").style.width = percentage + "%";
-    document.getElementById("loadingPercentage").textContent = percentage + "%";
-  }
-
-  function resetLoadingBar() {
-    document.getElementById("loadingBar").style.width = "0%";
-    document.getElementById("loadingPercentage").textContent = "0%";
-  }
+  question.addEventListener("focus", function () {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+    this.classList.remove("initial-height");
+  });
+  question.addEventListener("blur", function () {
+    this.style.height = "4em";
+    this.classList.add("initial-height");
+  });
+  A.addEventListener("focus", function () {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+    this.classList.remove("initial-height");
+  });
+  A.addEventListener("blur", function () {
+    this.style.height = "4em";
+    this.classList.add("initial-height");
+  });
+  B.addEventListener("focus", function () {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+    this.classList.remove("initial-height");
+  });
+  B.addEventListener("blur", function () {
+    this.style.height = "4em";
+    this.classList.add("initial-height");
+  });
+  C.addEventListener("focus", function () {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+    this.classList.remove("initial-height");
+  });
+  C.addEventListener("blur", function () {
+    this.style.height = "4em";
+    this.classList.add("initial-height");
+  });
+  D.addEventListener("focus", function () {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+    this.classList.remove("initial-height");
+  });
+  D.addEventListener("blur", function () {
+    this.style.height = "4em";
+    this.classList.add("initial-height");
+  });
+  E.addEventListener("focus", function () {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+    this.classList.remove("initial-height");
+  });
+  E.addEventListener("blur", function () {
+    this.style.height = "4em";
+    this.classList.add("initial-height");
+  });
 
   submitButton.addEventListener("click", handleSubmit);
   clearButton.addEventListener("click", handleClear);
